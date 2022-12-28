0728857d99caf70d539bd6bb5ade806a3aae093d
Carlos Sousa <carlos.sousa@sintese.local>
2022-12-28 18:10:24 -0300

    alterações

--- EditorHTML/Menu.cs
+++ EditorHTML/Menu.cs
@@ -4,6 +4,14 @@ namespace EditorHtml
 {
     public static class Menu
     {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+        }
 
     }
 }
